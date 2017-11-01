ed15f62796517b744f53809c7fa2208e1772d277
tanzeelak <tnkhan8042@gmail.com>
2017-11-01 06:34:43 -0700

    separated map as its own component

---
+++ src/components/home.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import MapboxGL from 'mapbox-gl'
+
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoidGFuemVlbGEiLCJhIjoiY2o5OTh0enVlMHBtNjMybHM1a2FxZjd6NCJ9.BluyLPr5_3NWWF9dMwkmvA"
+});
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+            <Map
+              style="mapbox://styles/mapbox/streets-v9"
+              containerStyle={{
+                height: "100vh",
+                width: "100vw"
+              }}>
+                <Layer
+                  type="symbol"
+                  id="marker"
+                  layout={{ "icon-image": "marker-15" }}>
+                  <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+                </Layer>
+            </Map>
+
+            </div>
+        )
+    }
+}
+
+export default Home;
