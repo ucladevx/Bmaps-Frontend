2a372e161b574815c3f8333f574ffe3c7ff04f47
Helen Lee <helenhyewonlee@gmail.com>
2018-01-03 00:40:19 -0800

    updates to modal mobile

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,18 +1,15 @@
 function showModal(modal, features){
 	var prop = features.properties;
 	switch(modal){
-		case 'sign-up':
+		case 'event':
 		// formatDate(new Date(e.features[0].properties.start_time));
-			$('.sign-up-modal-container').removeClass('hide');
+			$('.event-modal-container').removeClass('hide');
 			document.getElementById("modal-image").src = prop.cover_picture;
 			document.getElementById("event-date").innerHTML = prop.start_time.toString().split(" ")[0] + ' ' + prop.start_time.toString().split(" ")[1];
 			document.getElementById("event-title").innerHTML = prop.event_name;
 			document.getElementById("event-time").innerHTML = prop.start_time.toString().split("|")[1];
 			document.getElementById("event-location").innerHTML = prop.venue.split("location")[1].split("street")[1].split("\"")[2];
 			document.getElementById("event-description").innerHTML = prop.description;
-			break;
-		case 'event':
-			$('.event-modal-container').removeClass('hide');
 			break;
 	}
 
@@ -21,11 +18,8 @@ function showModal(modal, features){
 
 function hideModal(modal){
 	switch(modal){
-		case 'sign-up':
-			$('.sign-up-modal-container').addClass('hide');
-			break;
 		case 'event':
-			$('event-modal-container').addClass('hide');
+			$('.event-modal-container').addClass('hide');
 			break;
 		case 'all':
 			$('.modal-container').addClass('hide');
@@ -34,7 +28,9 @@ function hideModal(modal){
 }
 
 var initModal = function(){
-	$('#overlay').click(hideModal);
+	$('#overlay').click(function(){
+		hideModal('all');
+	});
 	$('.modal-x').click(function(){
 		hideModal('all');
 	});
