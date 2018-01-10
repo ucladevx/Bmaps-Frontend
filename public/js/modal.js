c61a7f04ef89ca193380fd226e8403b53bb2b23a
Helen Lee <helenhyewonlee@gmail.com>
2018-01-10 11:39:43 -0800

    modal resizing

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,10 +2,7 @@ function showModal(modal, features){
 	var prop = features.properties;
 	switch(modal){
 		case 'event':
-			// console.log(prop);
-		// formatDate(new Date(e.features[0].properties.start_time));
 			$('.event-modal-container').removeClass('hide');
-			// $('.event-modal-container').addClass('modal-overlay');
 			document.getElementById("modal-image").src = prop.cover_picture;
 			document.getElementById("event-date").innerHTML = prop.start_time.toString().split(" ")[0] + ' ' + prop.start_time.toString().split(" ")[1];
 			document.getElementById("event-title").innerHTML = prop.event_name;
@@ -17,6 +14,13 @@ function showModal(modal, features){
 				document.getElementById("event-location").innerHTML = prop.venue.split("location")[1].split("street")[1].split("\"")[2];
 			}
 			document.getElementById("event-description").innerHTML = prop.description;
+			document.getElementById("event-description").style.height = document.getElementById("event-modal").clientHeight - document.getElementById("modal-image").clientHeight - document.getElementById("event-title").clientHeight - document.getElementById("event-modal").clientHeight * 0.25;
+			if (document.getElementById("event-modal").clientHeight > 560) {
+				document.getElementById("event-description").style.height = document.getElementById("event-modal").clientHeight - document.getElementById("modal-image").clientHeight - document.getElementById("event-title").clientHeight - document.getElementById("event-modal").clientHeight * 0.25;
+			}
+			else {
+				document.getElementById("event-description").style.height = document.getElementById("event-modal").clientHeight - document.getElementById("modal-image").clientHeight - document.getElementById("event-title").clientHeight - document.getElementById("event-modal").clientHeight * 0.27;
+			}
 			// console.log(document.getElementById("event-description").clientHeight);
 			// console.log(document.getElementById("event-modal").clientHeight);
 			// if the right side is larger then make sure it is only 36% of the whole screen
@@ -31,9 +35,9 @@ function showModal(modal, features){
 			// 	// Click <a href="http://www.yahoo.com">here</a> to go to yahoo.
 			// }
 			// this is to make sure the cutting of the letters don't have issues
-			if (document.getElementById("event-description").clientHeight % 18 <= 15 && document.getElementById("event-description").clientHeight % 18 >= 3) {
-				document.getElementById("event-description").style.height = document.getElementById("event-description").clientHeight + (document.getElementById("event-description").clientHeight % 18);
-			}
+			// if (document.getElementById("event-description").clientHeight % 18 <= 15 && document.getElementById("event-description").clientHeight % 18 >= 3) {
+			// 	document.getElementById("event-description").style.height = document.getElementById("event-description").clientHeight + (document.getElementById("event-description").clientHeight % 18);
+			// }
 			
 			// break;
 	}
