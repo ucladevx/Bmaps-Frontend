558a25b13b557f85e2e26b01a6f48f9370f7b06a
tanzeelak <tnkhan8042@gmail.com>
2018-02-21 19:11:58 -0800

    basic list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/event.ts
@@ -0,0 +1,4 @@
+export class Event {
+  id: number;
+  name: string;
+}
