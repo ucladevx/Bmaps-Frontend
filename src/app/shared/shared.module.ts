cd499f0d607a922c28b498f141c4f9055456127a
Jorge Fuentes <jsjfuentesj@yahoo.com>
2018-02-25 20:01:54 -0800

    Add shared module for a singleton service

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+/*This shared module when imported by the app.module.ts provides all
+its service as SINGLETONS since its at the root making it the
+suggested place for utils and global varibles. At least that is what
+I gathered from https://angular.io/guide/singleton-services. Apparently
+will need to figure out forRoot if we have both declarations and providers
+*/
+
+//TODO: Actually check DateService is a singleton
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateService } from './date.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  providers: [DateService]
+})
+export class SharedModule {
+
+}
