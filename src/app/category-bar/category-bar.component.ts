bf4895e6a1b4d04d3ffaf2965be8ef77e94822fc
Sanketh Hegde <san2heg@gmail.com>
2018-03-06 13:08:43 -0800

    Minor changes

--- src/app/category-bar/category-bar.component.ts
+++ src/app/category-bar/category-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CategoryService } from '../category.service';
 import { EventService } from '../event.service';
