bd9120b579aca4f3becd6e17747daf0b51cf0a11
Sanketh Hegde <san2heg@gmail.com>
2018-03-09 02:35:15 -0800

    Make date visible in date bar

--- src/app/date-selector/date-selector.component.ts
+++ src/app/date-selector/date-selector.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { SelectedDate } from '../selectedDate';
 
 @Component({
   selector: 'app-date-selector',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-selector.component.css']
 })
 export class DateSelectorComponent implements OnInit {
+  private dateString: string;
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
 
   ngOnInit() {
+    console.log('inititin');
+    this.eventService.currDate$.subscribe(date => {
+      console.log('herefea');
+      this.dateString = this.dateToString(date);
+    });
   }
 
+  private dateToString(date: SelectedDate): string {
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth()+1;
+    let year = today.getFullYear();
+
+    return month.toString() + '/' + day.toString() + '/' + year.toString();
+  }
 }
