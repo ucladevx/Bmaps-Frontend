7613a803bbe0a9d7a5e59d183c0b0f59eba9afc0
tanzeelak <tnkhan8042@gmail.com>
2018-04-24 19:14:49 -0700

    before bringing updates

--- src/app/category-bar/category-bar.component.ts
+++ src/app/category-bar/category-bar.component.ts
@@ -13,7 +13,7 @@ import { NgClass } from '@angular/common';
 export class CategoryBarComponent implements OnInit {
   private categories;
   private events: GeoJson[];
-  public selectedCategory = "all categories";
+  public selectedCategory = 'all categories';
 
   constructor(private categService: CategoryService, private eventService: EventService) { }
 
@@ -25,14 +25,14 @@ export class CategoryBarComponent implements OnInit {
   }
 
   updateCategories(): void {
-    console.log("UPDATING CATEGORIES");
+    console.log('UPDATING CATEGORIES');
     this.categService.getCategories()
     .subscribe(categs => {
       let eventMap = this.getEventMap();
       this.categories = [{
-        category: "all",
-        formattedCategory: "all",
-        numEvents: eventMap["all"]
+        category: 'all',
+        formattedCategory: 'all',
+        numEvents: eventMap['all']
       }];
       for (let categ of categs.categories) {
         let categName = categ.category.toLowerCase();
@@ -52,8 +52,8 @@ export class CategoryBarComponent implements OnInit {
     let total = 0;
     for (let event of this.events) {
       let eventCateg: string = event.properties.category.toLowerCase();
-      if (eventMap[eventCateg] == undefined) {
-        eventMap[eventCateg] = 1
+      if (eventMap[eventCateg] === undefined) {
+        eventMap[eventCateg] = 1;
       }
       else {
         eventMap[eventCateg]++;
@@ -65,7 +65,7 @@ export class CategoryBarComponent implements OnInit {
   }
 
   filter(category: string): void {
-    if (category === "all") this.selectedCategory = "all categories";
+    if (category === 'all') this.selectedCategory = 'all categories';
     else this.selectedCategory = category.toLowerCase().replace('_', ' ');
     category = category.replace(' ', '_');
     this.eventService.filterEvents(category);
