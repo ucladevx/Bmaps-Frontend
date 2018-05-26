5074d304bd2308437e2daa0fbc2f36ae2f6023e9
Sanketh Hegde <san2heg@gmail.com>
2018-05-25 20:34:16 -0700

    Update API URL to live

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -49,8 +49,8 @@ export class EventService {
   private _categHash;
 
   // private baseUrl = "https://www.mappening.io/api/v1/events";
-  // private baseUrl = "https://www.mappening.io/api/v2/events"
-  private baseUrl = "http://0.0.0.0:5000/api/v2/events"
+  private baseUrl = "https://www.mappening.io/api/v2/events"
+  // private baseUrl = "http://0.0.0.0:5000/api/v2/events"
 
   constructor(private http: HttpClient, private dateService: DateService, private categService: CategoryService) {
     let today = new Date();
