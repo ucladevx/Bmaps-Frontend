e55b6018a1c619f9c1f4240ce8f3299c2ee4ba96
Sanketh Hegde <san2heg@gmail.com>
2018-05-29 19:05:56 -0700

    Delete duplicate API url

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -13,7 +13,6 @@ const httpOptions = {
 @Injectable()
 export class CategoryService {
   // private apiUrl = "https://www.mappening.io/api/v1/events/event-categories";
-  // private apiUrl = "http://www.mappening.io/api/v2/events/categories";
   // private apiUrl = "http://0.0.0.0:5000/api/v2/events/categories";
   private apiUrl = "https://www.mappening.io/api/v2/events/categories";
 
