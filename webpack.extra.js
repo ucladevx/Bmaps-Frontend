67aaa68bdced189b12e2c2f0a06dedf8a0e370e7
Ryan <rholland148@gmail.com>
2020-02-26 10:22:24 -0800

    Add webpack optimizations

--- webpack.extra.js
+++ webpack.extra.js
@@ -1,6 +1,52 @@
 const webpack = require('webpack');
+const path = require('path')
+const glob = require('glob-all')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
+
 module.exports = {
+  entry: {
+    main: './src/main.ts',
+    polyfill: './src/polyfills.ts'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      }
+    ]
+  },
   plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+    }),
   ]
 }
